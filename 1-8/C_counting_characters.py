a9360c04bd7b0c6d26d7dc9deff64a0e6a0aea92
daisuke23 <daisuke23bubu@gmail.com>
2022-06-14 19:46:00 +0900

    add problem

---
+++ 1-8/C_counting_characters.py
@@ -0,0 +1,19 @@
+'''
+与えられた文字列の列に含まれる、各アルファベットの数を数えるプログラムを作成して下さい。 なお、小文字と大文字は区別しません。
+'''
+while True:
+    try:
+        str = input()
+    except EOFError:
+        break
+str = str.lower()
+cnt = [0]*26
+for ch in str.lower():
+    try:
+        num = ord(ch) - ord('a')
+        cnt[num] += 1
+    except IndexError:
+        pass
+
+for i,ch in enumerate(cnt):
+    print(f"{chr(ord('a')+i)} : {ch}")
